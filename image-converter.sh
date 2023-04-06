cbd25e9c3b53534aebd15cba556c8483642070cc
Anton Nystedt <anton.nystedt@hotmail.com>
2023-04-06 10:58:07 +0200

    update to image-script

--- image-converter.sh
+++ image-converter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-output=${1-"avif"}
+output=${1-"webp"}
 
 # Set the input and output folder paths
 input_folder="."
@@ -13,8 +13,7 @@ for input_file in "$input_folder"/*.png; do
     filename=$(basename "$input_file")
 
     # Run the FFmpeg command to convert the PNG to AVIF
-    echo "npx @squoosh/cli --$output auto $input_file"
     npx @squoosh/cli --$output auto $input_file
-    echo "Converted $input_file to avif"
+    echo "Converted $input_file to $output"
   fi
 done
