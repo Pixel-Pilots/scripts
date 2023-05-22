36f9bcc34a3e579b3828c4f4d9174f9e7cd9bae4
Anton Nystedt <anton@pixelpilots.se>
2023-05-22 10:13:39 +0200

    Updated readme and made primary key script generic

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # scripts
+
+## `scripts` is a collection of scripts that I use to make my life easier.
+
+## Bash
+
+### image-converter.sh
+
+This script converts all images in a folder to another file format.
+Usage example: `bash image-converter.sh --dir ~/test --out avif`
+Defaults to current folder, and png -> webp
+
+## Python
+
+### remove-primary-keys.py
+
+Converts a dictionary object into an array of objects, and removes the primary key from the object.
+Usage example: `python3 remove-primary-keys.py -i /test/currencies.json -o output.json`
--- remove-primary-keys.py
+++ remove-primary-keys.py
@@ -1,14 +1,38 @@
 import json
+import sys
+import getopt
+
+arg_input = "input.json"
+arg_output = "output.json"
+
+opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["input=", "output="])
+
+for opt, arg in opts:
+    if opt in ("-i", "--input"):
+        arg_input = arg
+        # Update output file path
+        path = arg.split('/')
+        del path[-1]
+        path = '/'.join(map(str,path)) + '/'
+        arg_output = path + arg_output
+    elif opt in ("-o", "--output"):
+        path = arg_input.split('/')
+        del path[-1]
+        path = '/'.join(map(str,path)) + '/'
+        arg_output = path + arg
+
+print("Input file: " + arg_input)
+print("Output file: " + arg_output)
 
 # Load JSON file
-with open('currencies.json', 'r') as file:
+with open(arg_input, 'r') as file:
     data = json.load(file)
 
 # Remove primary keys
 data = list(data.values())
 
 # Save the converted JSON as an array of objects
-with open('output.json', 'w') as file:
+with open(arg_output, 'w') as file:
     json.dump(data, file)
 
 print("JSON dictionary converted to an array of objects.")
